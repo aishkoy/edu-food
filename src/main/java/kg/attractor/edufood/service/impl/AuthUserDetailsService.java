d742f3ff371a2121c94e497eeb14597053948580
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-05-02 14:38:58 +0600

    Add custom UserDetails and UserDetailsService implementation
    Introduce `CustomUserDetails` and `AuthUserDetailsService` to integrate with Spring Security.

---
+++ src/main/java/kg/attractor/edufood/service/impl/AuthUserDetailsService.java
@@ -0,0 +1,24 @@
+package kg.attractor.edufood.service.impl;
+
+import kg.attractor.edufood.entity.User;
+import kg.attractor.edufood.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class AuthUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new CustomUserDetails(user);
+    }
+}
