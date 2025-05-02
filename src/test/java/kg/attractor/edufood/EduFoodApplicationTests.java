39890a32d71a2f7f59a85b6e218e3ee3dfefd383
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-05-02 13:49:24 +0600

    Initial commit

---
+++ src/test/java/kg/attractor/edufood/EduFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.edufood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
