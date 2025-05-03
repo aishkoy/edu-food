2582c8adf656a707a3a5555fbf681dede916e53f
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2025-05-03 09:25:47 +0600

    feat: created additional query

--- src/main/java/kg/attractor/edufood/repository/OrderProductRepository.java
+++ src/main/java/kg/attractor/edufood/repository/OrderProductRepository.java
@@ -3,11 +3,20 @@ package kg.attractor.edufood.repository;
 import kg.attractor.edufood.entity.OrderProduct;
 import kg.attractor.edufood.entity.OrderProductId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OrderProductRepository extends JpaRepository<OrderProduct, OrderProductId> {
     List<OrderProduct> findAllByOrderId(Long orderId);
+    Optional<OrderProduct> findByOrderIdAndProductId(Long orderId, Long productId);
+
+    @Modifying
+    @Query("delete from OrderProduct op where op.order.id = :orderId and op.product.id = :productId")
+    void deleteByOrderIdAndProductId(@Param("orderId") Long orderId, @Param("productId") Long productId);
 }
--- src/main/java/kg/attractor/edufood/repository/OrderRepository.java
+++ src/main/java/kg/attractor/edufood/repository/OrderRepository.java
@@ -4,12 +4,15 @@ import kg.attractor.edufood.entity.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
@@ -20,6 +23,19 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     Page<Order> findByUserIdAndDateBefore(Long userId, Timestamp date, Pageable pageable);
 
     Page<Order> findByUserIdAndDateBetween(Long userId, Timestamp dateFrom, Timestamp dateTo, Pageable pageable);
+
+    Optional<Order> findByUserIdAndStatusId(Long userId, Long statusId);
+
+    @Modifying
+    @Query(value = """
+        update ORDERS
+            set TOTAL_AMOUNT = :totalAmount,
+                TOTAL_QUANTITY = :totalQuantity
+            where ID = :orderId
+        """, nativeQuery = true)
+    void update(@Param("orderId") Long orderId,
+                @Param("totalAmount") BigDecimal totalAmount,
+                @Param("totalQuantity") int totalQuantity);
 
     @Query(value = """
             select * from ORDERS o
